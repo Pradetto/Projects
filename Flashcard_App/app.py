67d6cf8fabb9c43913da9a8d35a1b6d4a928c643
Michael Pradetto <pradetto5@gmail.com>
2022-03-15 17:35:33 -0700

    Linked SQLite

--- Flashcard_App/app.py
+++ Flashcard_App/app.py
@@ -1,12 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, url_for,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+# import os
+
 
 app = Flask(__name__) # just referencing this file
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///flashcards.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Flashcards(db.Model):
+
+    name = db.Column(db.String(200), primary_key=True) 
+    question = db.Column(db.String(500),nullable=False) #nullable means user cannot leave it blank
+    answer = db.Column(db.String(500),nullable=False) #nullable means user cannot leave it blank
+
+    def __repr__(self):
+        return f'<Dataset {self.name}>'
+
+# db.create_all() # this line create the database file
+
+
+@app.route('/',methods = ['POST','GET'])
 def index():
-    return "Hello, World!"
+    if request.method == 'POST':
+        task_content = request.form["content"]
+        new_task = Flashcards(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your flashcard"
+
+    else:
+        tasks = Flashcards.query.all()
+        return render_template("index.html",tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-    
+
+
+# Conencted with SQLite remember to run it type sqlite3 in the terminal and to quit hit ctrl-c twice
+
+
+#Start interactive python3 shell just typing python3 in terminal
