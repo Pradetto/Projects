a35c09b6a9310050cf8d4616d5e4832c129c21d8
Michael Pradetto <pradetto5@gmail.com>
2022-03-21 20:43:27 -0700

    Setting up DB

--- Blog/app.py
+++ Blog/app.py
@@ -1,7 +1,29 @@
-from crypt import methods
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# sqlite::/// this is relative path sq
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite::///Users/michaelpradetto/Desktop/Projects/Blo/posts.db'
+db = SQLAlchemy(app)
+
+db.create_all()
+
+
+class BlogPost(db.Model):
+    # main distinguisher between table it's unique
+    id = db.Column(db.Integer, primary_key=True)
+    # string is limited 100 character and column can't be null
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    # says author is required but if not there set it to N/A
+    author = db.Column(db.String(20), nullable=False, default="N/A")
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog Post' + str(self.id)
+
 
 all_posts = [
     {
